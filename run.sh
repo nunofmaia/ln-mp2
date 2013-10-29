e99d5079c1d60edd0247d58d016348166acd9788
Nuno Maia <nunofmaia@gmail.com>
2013-10-29 22:08:12 +0000

    Change file names and review some details.

--- run.sh
+++ run.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra1/corpus.txt > palavra1/palavra1Anotado.txt
 
-python prepare.py  palavra1/palavra1Anotado.txt palavra1/palavra1Ambiguidade.txt  > palavra1/palavra1Bigramas.txt
+# PALAVRA 1
 
-sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra1/corpus_input.txt > palavra1/palavra1InputAnotado.txt
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra1/palavra1Anotado.txt > palavra1/palavra1NOR.txt
 
-python program.py palavra1/palavra1Bigramas.txt palavra1/palavra1Ambiguidade.txt palavra1/palavra1InputAnotado.txt > palavra1/palavra1Resultado.txt
+python prepare.py  palavra1/palavra1NOR.txt palavra1/palavra1Ambiguidade.txt  > palavra1/palavra1Bigramas.txt
 
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra1/corpus_input.txt > palavra1/palavra1Frases.txt
 
-sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra2/corpus.txt > palavra2/palavra2Anotado.txt
-
-python prepare.py  palavra2/palavra2Anotado.txt palavra2/palavra2Ambiguidade.txt  > palavra2/palavra2Bigramas.txt
-
-sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra2/corpus_input.txt > palavra2/palavra2InputAnotado.txt
-
-python program.py palavra2/palavra2Bigramas.txt palavra2/palavra2Ambiguidade.txt palavra2/palavra2InputAnotado.txt > palavra2/palavra2Resultado.txt
+python program.py palavra1/palavra1Bigramas.txt palavra1/palavra1Ambiguidade.txt palavra1/palavra1Frases.txt > palavra1/palavra1Resultado.txt
 
 
-sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra3/corpus.txt > palavra3/palavra3Anotado.txt
+# PALAVRA 2
 
-python prepare.py  palavra3/palavra3Anotado.txt palavra3/palavra3Ambiguidade.txt  > palavra3/palavra3Bigramas.txt
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra2/palavra2Anotado.txt > palavra2/palavra2NOR.txt
 
-sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra3/corpus_input.txt > palavra3/palavra3InputAnotado.txt
+python prepare.py  palavra2/palavra2NOR.txt palavra2/palavra2Ambiguidade.txt  > palavra2/palavra2Bigramas.txt
 
-python program.py palavra3/palavra3Bigramas.txt palavra3/palavra3Ambiguidade.txt palavra3/palavra3InputAnotado.txt > palavra3/palavra3Resultado.txt
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra2/corpus_input.txt > palavra2/palavra2Frases.txt
 
+python program.py palavra2/palavra2Bigramas.txt palavra2/palavra2Ambiguidade.txt palavra2/palavra2Frases.txt > palavra2/palavra2Resultado.txt
+
+
+# PALAVRA 3
+
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra3/palavra3Anotado.txt > palavra3/palavra3NOR.txt
+
+python prepare.py  palavra3/palavra3NOR.txt palavra3/palavra3Ambiguidade.txt  > palavra3/palavra3Bigramas.txt
+
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra3/corpus_input.txt > palavra3/palavra3Frases.txt
+
+python program.py palavra3/palavra3Bigramas.txt palavra3/palavra3Ambiguidade.txt palavra3/palavra3Frases.txt > palavra3/palavra3Resultado.txt
 
