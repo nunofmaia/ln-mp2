e510eb40aace30d4ef4986e416980347e8d13427
Nuno Maia <nunofmaia@gmail.com>
2013-10-29 18:07:10 +0000

    Add palavra3 sentences to test.

--- run.sh
+++ run.sh
@@ -2,18 +2,28 @@
 
 sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra1/corpus.txt > palavra1/palavra1Anotado.txt
 
-python cenas.py  palavra1/palavra1Anotado.txt palavra1/palavra1Ambiguidade.txt  > palavra1/palavra1Bigramas.txt
+python prepare.py  palavra1/palavra1Anotado.txt palavra1/palavra1Ambiguidade.txt  > palavra1/palavra1Bigramas.txt
 
 sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra1/corpus_input.txt > palavra1/palavra1InputAnotado.txt
 
-python program.py palavra1/palavra1Bigramas.txt palavra1/palavra1Ambiguidade.txt palavra1/palavra1InputAnotado.txt
+python program.py palavra1/palavra1Bigramas.txt palavra1/palavra1Ambiguidade.txt palavra1/palavra1InputAnotado.txt > palavra1/palavra1Resultado.txt
 
 
 sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra2/corpus.txt > palavra2/palavra2Anotado.txt
 
-python cenas.py  palavra2/palavra2Anotado.txt palavra2/palavra2Ambiguidade.txt  > palavra2/palavra2Bigramas.txt
+python prepare.py  palavra2/palavra2Anotado.txt palavra2/palavra2Ambiguidade.txt  > palavra2/palavra2Bigramas.txt
 
 sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra2/corpus_input.txt > palavra2/palavra2InputAnotado.txt
 
-python program.py palavra2/palavra2Bigramas.txt palavra2/palavra2Ambiguidade.txt palavra2/palavra2InputAnotado.txt
+python program.py palavra2/palavra2Bigramas.txt palavra2/palavra2Ambiguidade.txt palavra2/palavra2InputAnotado.txt > palavra2/palavra2Resultado.txt
+
+
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' -e 's/\/ \?/\/\?/g' < palavra3/corpus.txt > palavra3/palavra3Anotado.txt
+
+python prepare.py  palavra3/palavra3Anotado.txt palavra3/palavra3Ambiguidade.txt  > palavra3/palavra3Bigramas.txt
+
+sed -Ee 's/[«»“”]/"/g' -e 's/([\.,?!:;"()])/ \1 /g' -e 's/([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])-([^a-zA-ZáàãéóíõâôêÁÀÃÉÓÍÕÂÊÔçÇ])/\1 - \2/g' -e 's/ +/ /g' -e 's/\. \. \./\.\.\./g' -e 's/- -/--/g' < palavra3/corpus_input.txt > palavra3/palavra3InputAnotado.txt
+
+python program.py palavra3/palavra3Bigramas.txt palavra3/palavra3Ambiguidade.txt palavra3/palavra3InputAnotado.txt > palavra3/palavra3Resultado.txt
+
 
